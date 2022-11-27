fa7b7a391c7ac431569013348620cd90f7fdea7b
maulanamaib <83963442+maulanamaib@users.noreply.github.com>
2022-11-27 17:40:55 +0700

    Update pendat.py

--- pendat.py
+++ pendat.py
@@ -30,6 +30,11 @@ footer {visibility: hidden;}
 </style>
 
 """
+
+
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">', unsafe_allow_html=True)
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+st.markdown(' <div style="position: fixed; top: 0; left: 0; z-index: 9999; width: 100%; background: rgb(14, 17, 23); ; text-align: center;"><a href="https://github.com/LALA09-erha/Streamlit---WebPrediksiKNN" target="_blank"><button style="border-radius: 12px;position: relative; top:50%; margin:10px;"><i class="fa fa-github"></i> Source Code</button></a><a href="https://lala09-erha.github.io/datamining/intro.html" target="_blank"><button  style="border-radius: 12px;position: relative; top:50%;"><i style="color: orange" class="fa fa-book"></i> Jupyter Book</button></a></div>', unsafe_allow_html=True)
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 # colum = st.columns((0.1,10,1.5))
